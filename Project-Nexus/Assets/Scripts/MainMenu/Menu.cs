adf5c4bba9bf899cb30219cea423d774d609f5ae
Imotep460 <hasse_augu@hotmail.dk>
2022-01-01 08:18:21 +0100

    MainMenu button navigation.
    Created new folder for MainMenuScripts. Created the MainMenu script which has logic for button navigation between the mainMenuScreen, CreatelobbyScreen and the LobbyBrowserScreen.
    LobbyBrowserScreen still lacks logic to show available lobbies.

---
+++ Project-Nexus/Assets/Scripts/MainMenu/Menu.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Menu : MonoBehaviourPunCallbacks, ILobbyCallbacks
+{
+    // Reference the Menu Screens.
+    [Header("Screens")]
+    public GameObject mainMenuScreen;
+    public GameObject createLobbyScreen;
+    public GameObject LobbyScreen;
+    public GameObject LobbyBrowserScreen;
+
+    // Reference the Menu variables.
+    [Header("Main Screen")]
+    public Button createLobbyButton;    // Reference the buttons so that we can disable the buttons as needed to prevent
+    public Button findLobbyButton;      // Player from doing any network commands before we are connected to the master server.
+
+    [Header("Lobby")]
+    public TextMeshProUGUI playerListText;
+    public TextMeshProUGUI lobbyInfoText;
+    public Button startGameButton;
+
+    [Header("LobbyBrowser")]
+    public RectTransform lobbyListContainer;
+    public GameObject lobbyButtonPrefab;
+
+    private List<GameObject> lobbyButtons = new List<GameObject>();
+    private List<RoomInfo> lobbyList = new List<RoomInfo>();
+
+    private void Start()
+    {
+        // Disable the menu buttons at start so no network communication can take place before we have connected to the Master Server.
+        createLobbyButton.interactable = false;
+        findLobbyButton.interactable = false;
+
+        // Make sure the Cursor is enabled.
+        Cursor.lockState = CursorLockMode.None;
+
+        // Check if the Player is returning to the menu from a game.
+        if (PhotonNetwork.InRoom)
+        {
+            // Return the Player to the Lobby.
+
+            // Make the Lobby visible in the LobbyListScreen also make sure that the Lobby is open so people can join the Lobby.
+            PhotonNetwork.CurrentRoom.IsVisible = true;
+            PhotonNetwork.CurrentRoom.IsOpen = true;
+        }
+    }
+
+    /// <summary>
+    /// Switches the current visible screen.
+    /// </summary>
+    /// <param name="targetScreen">The Screen to be made visible.</param>
+    private void SetScreen(GameObject targetScreen)
+    {
+        // Disable all the Screens.
+        mainMenuScreen.SetActive(false);
+        createLobbyScreen.SetActive(false);
+        LobbyScreen.SetActive(false);
+        LobbyBrowserScreen.SetActive(false);
+
+        // Enable the target screen.
+        targetScreen.SetActive(true);
+    }
+
+    /// <summary>
+    /// Change the player nickname.
+    /// </summary>
+    /// <param name="playerNameInput">The new player nickname.</param>
+    public void OnPlayerNameChanged(TMP_InputField playerNameInput)
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    /// <summary>
+    /// Enable the menu buttons disabled in the Start method.
+    /// </summary>
+    public override void OnConnectedToMaster()
+    {
+        // Enable the menu buttons when a connection to the Master Server has been established.
+        createLobbyButton.interactable = true;
+        findLobbyButton.interactable = true;
+    }
+
+    /// <summary>
+    /// Switch to the createLobbyScreen.
+    /// </summary>
+    public void OnCreateLobbyButton()
+    {
+        SetScreen(createLobbyScreen);
+    }
+
+    /// <summary>
+    /// Switch to the LobbyBrowserScreen.
+    /// </summary>
+    public void OnFindLobbyButton()
+    {
+        SetScreen(LobbyBrowserScreen);
+    }
+
+    /// <summary>
+    /// Returns the Player to the MainMenuScreen.
+    /// </summary>
+    public void OnMainManuButton()
+    {
+        SetScreen(mainMenuScreen);
+    }
+
+    /// <summary>
+    /// Create a Lobby when the "Create" Button in the CreateLobbyScreen is pressed.
+    /// </summary>
+    /// <param name="lobbyNameInput">The name of the Lobby being created.</param>
+    public void OnCreateButton(TMP_InputField lobbyNameInput)
+    {
+        NetworkController.instance.CreateLobby(lobbyNameInput.text);
+    }
+}
