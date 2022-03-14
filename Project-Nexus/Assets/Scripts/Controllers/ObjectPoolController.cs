832057c193e6301c53344a5f9c8abf6972c2291d
Imotep460 <hasse_augu@hotmail.dk>
2022-03-14 10:13:38 +0100

    object pool

--- Project-Nexus/Assets/Scripts/Controllers/ObjectPoolController.cs
+++ Project-Nexus/Assets/Scripts/Controllers/ObjectPoolController.cs
@@ -1,84 +1,74 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using Photon.Pun;
-//using Photon.Realtime;
-//using ExitGames.Client.Photon;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
-//public class ObjectPoolController : MonoBehaviour, IPunPrefabPool
-//{
-//    //public static ObjectPoolController objectPoolInstance;
+public class ObjectPoolController : MonoBehaviour
+{
+    //public static ObjectPoolController objectPoolInstance;
 
-//    [Header("Objects")]
-//    public GameObject objectPrefab;
+    [Header("Objects")]
+    public GameObject objectPrefab;
 
-//    [Header("Variables")]
-//    public int createOnStart;
-//    private List<GameObject> objectPool = new List<GameObject>();
+    [Header("Variables")]
+    public int createOnStart;
+    private List<GameObject> objectPool = new List<GameObject>();
 
-//    // Start is called before the first frame update
-//    public void Start()
-//    {
-//        PhotonNetwork.PrefabPool = this;
-//        // Instantiate and add all the objects to the objectpool that the objectpool needs on start of a battle.
-//        for (int i = 0; i < createOnStart; i++)
-//        {
-//            CreateNewGameObject();
-//        }
-//    }
+    // Start is called before the first frame update
+    public void Start()
+    {
+        // Instantiate and add all the objects to the objectpool that the objectpool needs on start of a battle.
+        for (int i = 0; i < createOnStart; i++)
+        {
+            CreateNewGameObject();
+        }
+    }
 
-//    public GameObject Instantiate(string bulletPrefabId, Vector3 bulletPrefabPosition, Quaternion bulletPrefabRotation)
-//    {
-//        GameObject bulletObject = Instantiate(objectPrefab);
-//        bulletObject.transform.position = bulletPrefabPosition;
-//        bulletObject.transform.rotation = bulletPrefabRotation;
+    /// <summary>
+    /// Instantiate a new gameobject, add the new gameobject to the objectpool.
+    /// Gameobjects are by default disabled when they are added to the objectpool.
+    /// </summary>
+    /// <returns>Returns a reference to the newly created gameobject.</returns>
+    public GameObject CreateNewGameObject()
+    {
+        // Instantiate and maintain a reference to a new gameobject.
+        GameObject gameobject = PhotonNetwork.Instantiate("Bullet", new Vector3(0f, 0f, 0f), Quaternion.identity);
+        gameObject.SetActive(false);    // Disable the gameobject when it is first instantiated.
 
-//        return bulletObject;
-//    }
+        // Add the newly created object to the objectpool.
+        objectPool.Add(gameobject);
 
-//    /// <summary>
-//    /// Instantiate a new gameobject, add the new gameobject to the objectpool.
-//    /// Gameobjects are by default disabled when they are added to the objectpool.
-//    /// </summary>
-//    /// <returns>Returns a reference to the newly created gameobject.</returns>
-//    public GameObject CreateNewGameObject()
-//    {
-//        // Instantiate and maintain a reference to a new gameobject.
-//        GameObject gameobject = PhotonNetwork.Instantiate("Bullet", new Vector3(0f, 0f, 0f), Quaternion.identity);
-//        gameObject.SetActive(false);    // Disable the gameobject when it is first instantiated.
+        // Return the newly created gameobject.
+        return gameobject;
+    }
 
-//        // Add the newly created object to the objectpool.
-//        objectPool.Add(gameobject);
+    /// <summary>
+    /// Find a gameobject inside the objectpool.
+    /// Also check if the object is inactive.
+    /// </summary>
+    /// <returns>Returns a reference to a gameobject in the objectpool.</returns>
+    public GameObject GetObjectFromPool()
+    {
+        // Get a reference to an inactive gameobject in the objectpool.
+        GameObject tempGameObject = objectPool.Find(g => g.activeInHierarchy == false);
 
-//        // Return the newly created gameobject.
-//        return gameobject;
-//    }
+        if (tempGameObject == null)
+        {
+            // If there is no inactive gameobjects in the objectpool then we create a new gameobject and add it to the objectpool.
+            tempGameObject = CreateNewGameObject();
+        }
 
-//    /// <summary>
-//    /// Find a gameobject inside the objectpool.
-//    /// Also check if the object is inactive.
-//    /// </summary>
-//    /// <returns>Returns a reference to a gameobject in the objectpool.</returns>
-//    public GameObject GetObjectFromPool()
-//    {
-//        // Get a reference to an inactive gameobject in the objectpool.
-//        GameObject tempGameObject = objectPool.Find(g => g.activeInHierarchy == false);
+        //activate the gameobject.
+        tempGameObject.SetActive(true);
 
-//        if (tempGameObject == null)
-//        {
-//            // If there is no inactive gameobjects in the objectpool then we create a new gameobject and add it to the objectpool.
-//            tempGameObject = CreateNewGameObject();
-//        }
+        // Return a reference to the gameobject.
+        return tempGameObject;
+    }
 
-//        //activate the gameobject.
-//        tempGameObject.SetActive(true);
+    public void Destroy(GameObject bulletObject)
+    {
 
-//        // Return a reference to the gameobject.
-//        return tempGameObject;
-//    }
-
-//    public void Destroy(GameObject bulletObject)
-//    {
-        
-//    }
-//}
+    }
+}
